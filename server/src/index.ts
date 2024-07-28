5af76fefad52eaecdb3943d38301621220072bca
Daniel Loureiro <dmestreloureiro@gmail.com>
2024-07-28 20:14:45 -0300

    Mudança no port para 3001

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ AppDataSource.initialize().then(async () => {
 }).catch(error => console.log(error))
 
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api", routes);
